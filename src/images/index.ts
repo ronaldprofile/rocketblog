2591a3f7f958cf7375c65bdae4424e102d0ad05e
Ronald Tomaz <ronaldtmprofile@gmail.com>
2022-02-25 11:11:38 -0300

    exported all images into an object

---
+++ src/images/index.ts
@@ -0,0 +1,20 @@
+import logo from "./logo.svg";
+import arrowRightIcon from "./arrow-right.svg";
+import searchIcon from "./search.svg";
+
+import featuredImage from "./featured-image.png";
+import post1 from "./post-1.png";
+import post2 from "./post-2.png";
+import post3 from "./post-3.png";
+import post4 from "./post-4.png";
+
+export {
+  logo,
+  featuredImage,
+  arrowRightIcon,
+  searchIcon,
+  post1,
+  post2,
+  post3,
+  post4
+};
