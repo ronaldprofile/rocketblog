13d67c23db333721860ea05478f5be57dfd4dda8
Ronald Tomaz <ronaldtmprofile@gmail.com>
2022-03-05 21:12:12 -0300

    consuming data from posts

--- src/components/Posts/index.tsx
+++ src/components/Posts/index.tsx
@@ -1,9 +1,9 @@
-import { useInView } from "react-intersection-observer";
 import { motion } from "framer-motion";
+import { useInView } from "react-intersection-observer";
 
+import { getPosts } from "../../posts";
 import { Post } from "../../components/Post";
 
-import * as I from "../../images";
 import * as P from "./styles";
 
 const containerList = {
@@ -30,6 +30,7 @@ const itemList = {
 };
 
 export function Posts() {
+  const posts = getPosts();
   const { ref, inView } = useInView();
 
   return (
@@ -39,44 +40,21 @@ export function Posts() {
           variants={containerList}
           animate={inView ? "show" : "hidden"}
         >
-          <motion.li
-            variants={itemList}
-            animate={inView ? "show" : "hidden"}
-            custom={0}
-          >
-            <Post
-              date="Janeiro 04, 2022"
-              title="10 dicas para conseguir a vaga desejada"
-              description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nibh nibh eu in aliquet ut adipiscing neque. Sed volutpat aenean sit vitae, sed tristique. "
-              imageUrl={I.post2}
-            />
-          </motion.li>
-
-          <motion.li
-            variants={itemList}
-            animate={inView ? "show" : "hidden"}
-            custom={1}
-          >
-            <Post
-              date="Janeiro 04, 2022"
-              title="Deixe seu código mais semântico com essas dicas"
-              description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nibh nibh eu in aliquet ut adipiscing neque. Sed volutpat aenean sit vitae, sed tristique. "
-              imageUrl={I.post3}
-            />
-          </motion.li>
-
-          <motion.li
-            variants={itemList}
-            animate={inView ? "show" : "hidden"}
-            custom={2}
-          >
-            <Post
-              date="Janeiro 04, 2022"
-              title="Use essas dicas nas suas aplicações mobile"
-              description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nibh nibh eu in aliquet ut adipiscing neque. Sed volutpat aenean sit vitae, sed tristique. "
-              imageUrl={I.post4}
-            />
-          </motion.li>
+          {posts.map(post => (
+            <motion.li
+              key={post.id}
+              variants={itemList}
+              animate={inView ? "show" : "hidden"}
+              custom={post.customDelay}
+            >
+              <Post
+                date={post.date}
+                title={post.title}
+                description={post.description}
+                imageUrl={post.imageUrl}
+              />
+            </motion.li>
+          ))}
         </motion.ul>
       </div>
     </P.Container>
--- src/components/Posts/styles.ts
+++ src/components/Posts/styles.ts
@@ -3,15 +3,15 @@ import { styled } from "../../styles";
 export const Container = styled("section", {
   "& .wrapper": {
     ul: {
-      display: "flex",
-      alignItems: "center",
+      display: "grid",
+      gridTemplateColumns: "repeat(3, 1fr)",
 
       gap: 30
     },
 
     "@breakpoint4": {
       ul: {
-        flexDirection: "column"
+        gridTemplateColumns: "1fr"
       }
     }
   }
